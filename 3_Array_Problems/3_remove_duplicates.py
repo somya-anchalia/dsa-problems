11c20acf24197d7de682c071a58cb523a0ac5ade
Somya Anchalia <sanchalia96@gmail.com>
2024-06-04 15:56:50 +0530

    remove duplicates from an array

---
+++ 3_Array_Problems/3_remove_duplicates.py
@@ -0,0 +1,19 @@
+
+from typing import List
+
+def removeDuplicates(arr: List[int]) -> int:
+    i = 0
+    for j in range(1, len(arr)):
+        if arr[i] != arr[j]:
+            i += 1
+            arr[i] = arr[j]
+    return i + 1
+
+
+if __name__ == "__main__":
+    arr = [1, 1, 2, 2, 2, 3, 3]
+    k = removeDuplicates(arr)
+    print("The array after removing duplicate elements is ")
+    for i in range(k):
+        print(arr[i], end=" ")
+
