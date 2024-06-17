cc556fc8542f49b6c7f08be5215f4bf63643c78e
Somya Anchalia <sanchalia96@gmail.com>
2024-06-17 11:57:43 +0100

    find sum pairs

--- 3_Array_Problems/10_longest_subarray.py
+++ 3_Array_Problems/10_longest_subarray.py
@@ -1,6 +1,3 @@
-
-
-
 from typing import List
 
 def getLongestSubarray(a: [int], k: int) -> int:
---
+++ 3_Array_Problems/find_sum_pairs.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class find_sum_pairs {
+    public static String twoSum(int n, int []arr, int target) {
+        Arrays.sort(arr);
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (sum == target) {
+                return "YES";
+            } else if (sum < target) left++;
+            else right--;
+        }
+        return "NO";
+    }
+    public static void main(String args[]) {
+        int n = 5;
+        int[] arr = {2, 6, 5, 8, 11};
+        int target = 14;
+        String ans = twoSum(n, arr, target);
+        System.out.println("This is the answer for variant 1: " + ans);
+    }
+}
