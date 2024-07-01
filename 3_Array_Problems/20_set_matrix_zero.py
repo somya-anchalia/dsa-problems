914fe4c80190c7a528b3175eaccff40fddf8ea22
Somya Anchalia <sanchalia96@gmail.com>
2024-07-01 13:03:15 +0100

    Set matrix zero problem

---
+++ 3_Array_Problems/20_set_matrix_zero.py
@@ -0,0 +1,37 @@
+
+def zeroMatrix(matrix, n, m):
+    row = [0] * n  # row array
+    col = [0] * m  # col array
+
+    # Traverse the matrix:
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j] == 0:
+                # mark ith index of row wih 1:
+                row[i] = 1
+
+                # mark jth index of col wih 1:
+                col[j] = 1
+
+    # Finally, mark all (i, j) as 0
+    # if row[i] or col[j] is marked with 1.
+    for i in range(n):
+        for j in range(m):
+            if row[i] or col[j]:
+                matrix[i][j] = 0
+
+    return matrix
+
+
+if __name__ == "__main__":
+	matrix = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+	n = len(matrix)
+	m = len(matrix[0])
+	ans = zeroMatrix(matrix, n, m)
+
+	print("The Final matrix is:")
+	for row in ans:
+	    for ele in row:
+	        print(ele, end=" ")
+	    print()
+
