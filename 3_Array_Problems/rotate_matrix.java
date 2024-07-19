54ed9cec46483c099d10918946bc5434ff6894f5
Somya Anchalia <sanchalia96@gmail.com>
2024-07-19 17:53:11 +0100

    matrix rotation in java

---
+++ 3_Array_Problems/rotate_matrix.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+public class rotate_matrix {
+    static int[][] rotate(int[][] matrix) {
+        int n = matrix.length;
+        int rotated[][] = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rotated[j][n - i - 1] = matrix[i][j];
+            }
+        }
+        return rotated;
+    }
+
+    public static void main(String args[]) {
+        int arr[][] =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        int rotated[][] = rotate(arr);
+        System.out.println("Rotated Image");
+        for (int i = 0; i < rotated.length; i++) {
+            for (int j = 0; j < rotated.length; j++) {
+                System.out.print(rotated[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
