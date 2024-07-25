fe38eb1a1868dfe1073d3a1f5ac7eb108dfcde4e
Somya Anchalia <sanchalia96@gmail.com>
2024-07-25 22:12:13 +0100

    balanced parentheses problem

---
+++ 3_Array_Problems/27_balanced_parentheses.py
@@ -0,0 +1,25 @@
+def isValid(s: str) -> bool:
+    st = []
+    for it in s:
+        if it == '(' or it == '{' or it == '[':
+            st.append(it)
+        else:
+            if len(st) == 0:
+                return False
+            ch = st[-1]
+            st.pop()
+            if (it == ')' and ch == '(') or (it == ']' and ch == '[') or (it == '}' and ch == '{'):
+                continue
+            else:
+                return False
+    return len(st) == 0
+
+
+
+
+if __name__ == '__main__':
+    s = "()[{}()]"
+    if isValid(s):
+        print("True")
+    else:
+        print("False")
