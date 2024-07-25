14fb705c614c4a66e9bc630f5e947b145f99f7a5
Somya Anchalia <sanchalia96@gmail.com>
2024-07-25 22:19:43 +0100

    longest prefix problem

---
+++ 3_Array_Problems/28_longest_prefix.py
@@ -0,0 +1,15 @@
+from typing import List
+
+def longestCommonPrefix(strs: List[str]) -> str:
+
+    if len(strs) == 0:
+        return ""
+    prefix = strs[0]
+    for i in range(1, len(strs)):
+        while strs[i].find(prefix) != 0:
+            prefix = prefix[0 : len(prefix) - 1]
+            if prefix == "":
+                return ""
+    return prefix
+
+print(longestCommonPrefix(["flower","flow","flight"]))
