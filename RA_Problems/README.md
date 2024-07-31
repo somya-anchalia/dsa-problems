3f1221ef96cb3eacdf6c01d1b3b83e45fd9e4e74
Somya Anchalia <sanchalia96@gmail.com>
2024-07-31 18:33:54 +0100

    min cost tickets problem

--- RA_Problems/README.md
+++
@@ -1,2 +0,0 @@
-# dsa-problems
-Diving deep into DSA, with 450+ modules. C++, Java, Python, and Javascript code.
--- SA_Problems/Assessment practise/10_minCostTickets(AVAILITY).py
+++ SA_Problems/Assessment practise/10_minCostTickets(AVAILITY).py
@@ -1,1 +1,24 @@
 # https://leetcode.com/problems/minimum-cost-for-tickets/description/
+from typing import List
+from collections import defaultdict
+
+def mincostTickets(days: List[int], costs: List[int]) -> int:
+        dp = defaultdict(lambda: 0)
+        print(dp)
+        for d in range(1, days[-1] + 1):
+            dp[d] = (
+                min(
+                    dp[d - 1] + costs[0],
+                    dp[d - 7] + costs[1],
+                    dp[d - 30] + costs[2],
+                )
+                if d in days
+                else dp[d - 1]
+            )
+            print(dp)
+        return dp[days[-1]]
+
+days = [1,4,6,7,8,20]
+cost = [2,7,15]
+min_cost = mincostTickets(days,cost)
+print(min_cost)
