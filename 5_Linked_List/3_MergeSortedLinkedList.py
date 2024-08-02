e2d14795e1bcf3144bbe17221788eaef73b1c89d
Somya Anchalia <sanchalia96@gmail.com>
2024-08-02 19:08:52 +0100

    optimal solution for merging sorted linked list

--- 5_Linked_List/3_MergeSortedLinkedList.py
+++ 5_Linked_List/3_MergeSortedLinkedList.py
@@ -1,92 +1,59 @@
-                                
-# Node class represents a
-# node in a linked list
-class Node:
-    def __init__(self, data1, next1=None):
-        # Data stored in the node
-        self.data = data1
-        # Pointer to the next node in the list
-        self.next = next1
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, list1, list2):
+        dummy = ListNode()
+        cur = dummy
 
-# Function to convert a list to a linked list
-def convert_arr_to_linked_list(arr):
-    # Create a dummy node to serve
-    # as the head of the linked list
-    dummy_node = Node(-1)
-    temp = dummy_node
+        while list1 and list2:
+            if list1.val > list2.val:
+                cur.next = list2
+                list2 = list2.next
+            else:
+                cur.next = list1
+                list1 = list1.next
+            
+            cur = cur.next
+        
+        if list1:
+            cur.next = list1
+        else:
+            cur.next = list2
+        
+        return dummy.next
 
-    # Iterate through the list and
-    # create nodes with list elements
-    for i in range(len(arr)):
-        # Create a new node with the list element
-        temp.next = Node(arr[i])
-        # Move the temporary pointer
-        # to the newly created node
-        temp = temp.next
-
-    # Return the linked list starting
-    # from the next of the dummy node
-    return dummy_node.next
-
-# Function to merge two sorted linked lists
-def sort_two_linked_lists(list1, list2):
-    arr = []
-    temp1 = list1
-    temp2 = list2
-
-    # Storing elements of both lists into a list
-
-    # Add elements from list1 to the list
-    while temp1 is not None:
-        arr.append(temp1.data)
-        # Move to the next node in list1
-        temp1 = temp1.next
-
-    # Add elements from list2 to the list
-    while temp2 is not None:
-        arr.append(temp2.data)
-        # Move to the next node in list2
-        temp2 = temp2.next
-
-    # Sorting the list in ascending order
-    arr.sort()
-
-    # Converting the sorted list
-    # back to a linked list
-    head = convert_arr_to_linked_list(arr)
-
-    # Return the head of the
-    # merged sorted linked list
-    return head
-
-# Function to print the linked list
-def print_linked_list(head):
-    temp = head
-    while temp is not None:
-        # Print the data of the current node
-        print(temp.data, end=" ")
-        # Move to the next node
-        temp = temp.next
-    print()
+    # Function to print the linked list
+    def print_linked_list(self, head):
+        temp = head
+        while temp is not None:
+            # Print the data of the current node
+            print(temp.val, end=" ")
+            # Move to the next node
+            temp = temp.next
+        print()
 
 # Example Linked Lists
-list1 = Node(1)
-list1.next = Node(3)
-list1.next.next = Node(5)
+list1 = ListNode(1)
+list1.next = ListNode(3)
+list1.next.next = ListNode(4)
+list1.next.next.next = ListNode(5)
 
-list2 = Node(2)
-list2.next = Node(4)
-list2.next.next = Node(6)
+list2 = ListNode(2)
+list2.next = ListNode(4)
+list2.next.next = ListNode(6)
+list2.next.next.next = ListNode(7)
 
-print("First sorted linked list: ", end="")
-print_linked_list(list1)
+print("Linked list 1: ", end="")
+Solution().print_linked_list(list1)
 
-print("Second sorted linked list: ", end="")
-print_linked_list(list2)
+print("Linked list 2: ", end="")
+Solution().print_linked_list(list2)
 
-merged_list = sort_two_linked_lists(list1, list2)
+merged_list = Solution().mergeTwoLists(list1, list2)
 
 print("Merged sorted linked list: ", end="")
-print_linked_list(merged_list)
-                                
-                            
+Solution().print_linked_list(merged_list)
+
